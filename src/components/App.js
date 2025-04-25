6bcc673f919b3168b8d0bae8e88e63cb0af4ac4a
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-25 11:32:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+      <p>Button clicked {count} times</p>
+      <button onClick={()=> setCount(count+1)}>Click Me</button>
     </div>
   )
 }
