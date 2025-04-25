7c7dd958ef8c7a1a758459197e86a5a6fda9acd9
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-25 12:50:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [name, setName] = useState("");
-
-  return (
-    <>
-      <input type="text" name="name" id="name" placeholder='Enter Your Name' onChange={(e) => setName(e.target.value)}/>
-
-      <h1>Hello, {name}!</h1>
-    </>
-  )
-}
-
-export default App
+import React from "react";
+import { useState } from 'react';
+ import './../styles/App.css';
+ 
+ const App = () => {
+   const [name, setName] = useState("");
+   return (
+     <div>
+          <input type="text" name="name" id="name" placeholder='Enter Your Name' onChange={(e) => setName(e.target.value)}/>
+          <h1>Hello, {name}!</h1>
+     </div>
+   )
+ }
+ 
+ export default App
